5676aee08b2ccbea0e92e3abf52d013fa182d01b
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-11-15 21:53:15 +0800

    tweaked a little

--- index4.js
+++ index4.js
@@ -11,19 +11,20 @@ var bandName = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function bandNameGenerator(req, res, next) {
-  console.log(req.body);
-  bandName = req.body["street"] + " " + req.body["pet"];
-  next();
-}
+// function bandNameGenerator(req, res, next) {
+//   console.log(req.body);
+//   bandName = req.body["street"] + " " + req.body["pet"];
+//   next();
+// }
 
-app.use(bandNameGenerator);
+// app.use(bandNameGenerator);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/submit", (req, res) => {
+  bandName = req.body["street"] + " " + req.body["pet"];
   res.send(`<h1>Your bandname is: </h1> <h2>${bandName}</h2>`);
 });
 
